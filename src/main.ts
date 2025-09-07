0f81ddc10f071b0b69916ae65d10bcff5a88ac4c
fullstack_daddy <aboyejiemmanuel07@gmail.com>
2025-09-07 13:22:06 +0100

    SV-FIX(server port): Changed app.listen port

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
